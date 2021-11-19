aef653077f813ff1cbab360d6d7adc0b3964adde
Snigdha Das <snigdha@stat.tamu.edu>
2021-11-18 23:09:05 -0600

    Calculate the p-norm of a given vector in the header file.

--- src/CppBasics.h
+++ src/CppBasics.h
@@ -17,3 +17,16 @@ void vec_mem_cpy(IntegerVector from, IntegerVector to){
   for(int i=0; i<to.size(); i++)
     to[i] = from[i] ;
 }
+
+// Calculate the p-norm of a vector as ||x||_p = (\sum |x_i|^p)^(1/p)
+double norm(NumericVector x, int p=2, bool power=false){
+  double res=0.0 ;
+
+  // res = \sum |x_i|^p
+  for(int i=0; i<x.size(); i++)
+    res += pow(abs(x[i]), p) ;
+  if(power == true)
+    return(res) ;   //if power = true, return \sum |x_i|^p
+  else
+    return(pow(res, 1.0/(double)p)) ;   // otherwise return (\sum |x_i|^p)^(1/p)
+}
