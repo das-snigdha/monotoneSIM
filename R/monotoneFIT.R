35f074e889d7be6332af62bb6f36dca3367d391b
Snigdha Das <snigdha@stat.tamu.edu>
2021-12-05 17:58:15 -0600

    Add the code to estimate Y using posterior mean of beta.

--- R/monotoneFIT.R
+++ R/monotoneFIT.R
@@ -9,9 +9,20 @@ monotoneFIT = function(mono.sim, grid.x = NULL, size.grid.x = 100){
     grid.x = as.vector(grid.x)
   }
   xi.mtx = as.matrix(mono.sim$xi)
-  # return(xi.mtx)
+
   # Call c++ function g_mtx to calculate g(x) for the grid.x using xi's obtained from the MCMC.
   g.sample = g_mtx(xi_mtx = xi.mtx, grid_x = grid.x, u = mono.sim$knots)
 
+  # Estimate beta.Xscaled by the posterior sample mean.
+  est.beta.Xscaled = colMeans(mono.sim$beta.Xscaled)
+
+  #Use estimated beta and X.scaled to calculate Xbeta
+  Xbeta = mono.sim$X.scaled %*% est.beta.Xscaled
+
+  # Call c++ function g_mtx to calculate g(Xbeta) i.e. estimate Y using xi's obtained from the MCMC.
+  y.fitted = g_mtx(xi_mtx = xi.mtx, grid_x = Xbeta, u = mono.sim$knots)
+
+  # return y.fitted, g.sample and grid.x
+  return(list("Y.fitted" = y.fitted, "g.estimated" = g.sample, "grid.x" = grid.x))
 
 }
