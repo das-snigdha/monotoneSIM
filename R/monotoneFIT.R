0a45fa6fd46c06abbdb05964d6feb9efbc2201e0
Snigdha Das <snigdha@stat.tamu.edu>
2021-12-06 01:27:29 -0600

    Add description of parameters and returned values in Roxygen Skeleton of monotoneFIT().

--- R/monotoneFIT.R
+++ R/monotoneFIT.R
@@ -1,4 +1,17 @@
-monotoneFIT = function(mono.sim, grid.x = NULL, size.grid.x = 100){
+#' Extract Fitted values after Bayesian estimation of a Monotone Single Index Model.
+#'
+#' @param mono.sim A returned object of \code{monotoneSIM} function.
+#' @param size.grid.x length of vector of values for which the monotone function is estimated. Takes the value \eqn{100} by default.
+#' @param grid.x (Optional) Vector of user supplied values for which the monotone function is estimated. Takes \code{NULL} by default. If \code{NULL}, vector (of length size.grid.x) of equispaced values between -1 and 1 are chosen.
+#'
+#' @return A list with the following elements.
+#' \item{Y.fitted}{ \code{M} \eqn{x n} Matrix of Fitted values of the response. \code{M} is the size of the MCMC sample obtained in \code{monotoneSIM}. }
+#' \item{g.estimated}{ \code{M} \eqn{x} \code{size.grid.x} Matrix OR (if \code{grid.x} is supplied) \code{M} \eqn{x} \code{length(grid.x)} Matrix of estimated values of the link function. Each row represents estimated value of the link function evaluated on grid.x, corresponding to each posterior sample of basis coefficients.}
+#' \item{grid.x}{ Grid of x values at which the link function is estimated. }
+#' @export
+#'
+#' @examples
+monotoneFIT = function(mono.sim, size.grid.x = 100, grid.x = NULL){
 
   # If grid values of x are not provided, take a grid of size = size.grid.x with equispaced values from -1 and 1.
   if (is.null(grid.x)){
